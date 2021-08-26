8610a90b6f7a9e18832982f91b7c98285782b055
sturpin <turpiniko@gmail.com>
2021-08-26 18:22:08 +0200

    Add functions-arrow.js

---
+++ functions-arrow.js
@@ -0,0 +1,10 @@
+//----------- FUNCTIONS ARROW -----------//
+
+const sumar = ( a, b ) => a + b;
+console.log( sumar(5,5) ); // => 10
+
+
+// Si la función no recibe ningún argumento
+
+const saludar = () => 'Hola amigo ;)';
+console.log( saludar() ); // => Hola amigo ;)
